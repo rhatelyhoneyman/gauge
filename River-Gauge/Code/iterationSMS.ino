3fcf01bafdc510e416c656d11a0c6cbe7642578d
rhatelyhoneyman <113561145+rhatelyhoneyman@users.noreply.github.com>
2024-04-09 23:25:27 -0700

    Update iterationSMS.ino
    added Arduino Cloud functions + info about implementing cloud variable in header

--- River-Gauge/Code/iterationSMS.ino
+++ River-Gauge/Code/iterationSMS.ino
@@ -1,16 +1,23 @@
 /*******************************TODO***************************************************
-Error handling
-go over it and reformat code
-saying if message is invalid or too long
-sms flush / board memeory/ cache wiping (or if that even needs to happen)
+Error handling --> line 55/56
+go over it and reformat code --> yep
+saying if message is invalid or too long --> unneeded?
+create thing (cloud variable)
+-Use link: https://create.arduino.cc/iot/things
+-create basic integer type variable with updates every 3600s called distance(or whatever)
+-use code structure below with board plugged in
+-thingProperties.h is created with thing, network info, and defines initProperties()
+-sample code: https://forum.arduino.cc/t/initialize-properties-on-arduino-power-up/664152/2
 figure error correction of ultrasonic reading to make sure its accurate/consistent
-test test
 eventually adapt it for longer use and potentially automating it to database or csv
 ***************************************************************************************/
 
 #include <string>
 #include <MKRGSM.h>
-#include <iostream>    
+#include <iostream>
+#include "thingProperties.h" //library for arduino cloud variables
+
+
 //function reminders
 //available (): checks sms messages are on sim card, returns quantity of characters as an integer - SMS.available()
 //remoteNumber(): retrieves phone # from incoming message and stores it in array - SMS.remoteNumber(number, size)
@@ -29,7 +36,8 @@ GSM_SMS sms;
 char PINNUMBER[10] = secretpinnumber; //public mobile sim card pin
 
 void setup(){
-
+    Serial.begin(9600); //check if required (its in sample code)
+    
     //  connection  state
     bool  connected  =  false;
 
@@ -40,9 +48,16 @@ void setup(){
             delay(10);
         }
     }
+    initProperties(); //defined in thingProperties.h
+    ArduinoCloud.begin(ArduinoIoTPreferredConnection); //connects to Arduino Cloud
+    ArduinoCloud.addCallback(ArduinoIoTCloudEvent::SYNC, onIoTSync); //Initialize cloud variables
+    //debugging function, not required, set level up to 4 for "more granular information"
+    setDebugMessageLevel(2);
+    ArduinoCloud.printDebugInfo();
 }
 
 void loop() {
+    ArduinoCould.update();
 
   pinMode(echoPin, INPUT);
   pinMode(trigPin, OUTPUT);
@@ -112,4 +127,9 @@ int distanceMeasure (){
       }
       avg_distance  =  summed_distance  /  count;
       return  avg_distance;
+}
+void onIoTSync(){
+//initialize distance variable (same name as thing)
+nameThing = distanceMeasure(); or //nameThing.distanceMeasure(); but not 100% sure
+Serial.println("Arduino synced to IoT Cloud"); //debug
 }
